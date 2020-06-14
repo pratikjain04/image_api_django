3f7e4d8d9aeff6600a9fa389657e8c1f49db502f
pratikjain04 <pratikjain0411@gmail.com>
2020-06-14 19:46:53 +0530

    added computerviision model
    Signed-off-by: pratikjain04 <pratikjain0411@gmail.com>

---
+++ computervision/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ComputervisionConfig(AppConfig):
+    name = 'computervision'
--- deaf/views.py
+++ deaf/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets
 from . models import ImageUploadModel
 from . serializer import FileSerializer
 from django.shortcuts import render
-
+from rest_framework import generics
 
 class FileUploadView(viewsets.ModelViewSet):
     queryset = ImageUploadModel.objects.all()
